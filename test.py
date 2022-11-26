eeba1b130b75ea7026b09b7b098f6f8fa8bbf9f7
Eric Landgraf <elandg@umich.edu>
2022-11-26 12:49:54 -0500

    added more not working test files

---
+++ test.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from chromedriver_py import binary_path # this will get you the path variable
+
+service_object = Service(binary_path)
+driver = webdriver.Chrome(service=service_object)
+
+# deprecated but works in older selenium versions
+# driver = webdriver.Chrome(executable_path=binary_path)
+driver.get("http://www.python.org")
+assert "Python" in driver.title
